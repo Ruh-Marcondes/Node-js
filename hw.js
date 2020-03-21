6d59005080a101488001b21a84688e09c29a9c8b
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2020-03-20 23:27:10 -0300

    aprendendo como funciona o *node* js

---
+++ hw.js
@@ -0,0 +1,8 @@
+console.log("Hello World! - lobister")//printa na tela
+
+// declaração de variaveis
+var nome = "R."
+var sobrenome = "Bravo"
+
+// concatenação de duas váriaveis do tipo str
+console.log("\n"+nome +" "+sobrenome);

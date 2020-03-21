24fb4bd7a08920b186d0d07ded2d92920ccee114
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2020-03-20 23:42:00 -0300

    chamando os modulos e testando

--- Modulos(calc)/calc.js
+++ Modulos(calc)/calc.js
@@ -1,1 +1,11 @@
+// importar as funcoes 
+var somaf = require("./somar")
+var subf = require("./sub")
+var divf = require("./div")
+var multf = require("./mult")
 
+
+console.log(somaf(1,2));
+console.log("\n" + subf(2,1))
+console.log("\n" + multf(100.25,5))
+console.log("\n"+ divf(55,5))

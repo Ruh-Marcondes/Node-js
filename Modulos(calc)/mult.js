8f08c205d8c08e2ec687b1352320122ae9784a34
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2020-05-20 20:50:42 -0300

    excluindo o q n me interresa  o(_ _)o

--- Modulos(calc)/mult.js
+++
@@ -1,7 +0,0 @@
-var multi = function (a,b){
-
-    return a*b;
-}
-
-// modulo de exportação
-module.exports = multi;
--- hw.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello World! - lobister")//printa na tela
-
-// declaração de variaveis
-var nome = "R."
-var sobrenome = "Bravo"
-
-// concatenação de duas váriaveis do tipo str
-console.log("\n"+nome +" "+sobrenome);

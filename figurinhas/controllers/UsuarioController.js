e8a949817502a1026ff12b7b6139f1817d5a76ed
Ruh-Marcondes <marcondes.ruahma@gmail.com>
2025-03-12 12:00:01 -0300

    Só me pergo nessa jossa

--- figurinhas/controllers/UsuarioController.js
+++ figurinhas/controllers/UsuarioController.js
@@ -17,8 +17,35 @@ exports.create = (req, res, next) => {
         bcrypt.hash(senha,10)
         .then(senhaCriptografada =>{
             Usuario.findOne({
-                
+
             })
         })
     }
+}
+
+exports.update = (req,res,next) =>{
+    const nome = req.body.nome;
+    const email = req.body.email;
+    const senha  = req.body.senha;
+
+
+}
+
+exports.getAll = (req,res,next) =>{
+    Usuario.findAll({
+        order:[
+            ['nome','ASC']
+        ],
+        attributes:['id','nome','email']
+    }).then(usuarios =>{
+        res.status(200).json({
+            mensagem:'Usuario não encontrado'
+        });
+    }).catch();
+    
+}
+
+exports.getOne = (req,res,next)=>{
+
+    
 }
